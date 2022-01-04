25bf92e405893f1a2d4954fac4a0be8935210a79
Asraful <asrafulapf@gmail.com>
2022-01-04 06:54:05 +0600

    date changed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Jun 16, 2022 00:00:00").getTime();
+let launchDate = new Date("Jun 14, 2022 00:00:00").getTime();
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
 
